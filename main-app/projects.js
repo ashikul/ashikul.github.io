9bcd74644a0a24111543f7e69b9d9a828e79b55e
Ashikul <alam.ashikul@gmail.com>
2016-05-12 21:49:38 -0400

    portfolio redesign

--- main-app/projects.js
+++ main-app/projects.js
@@ -2,67 +2,78 @@ var projects = [
     {
         "title": "Github Profile Viewer",
         "link": "http://Ashikul.github.io/githubprofile",
-        "img": "images/gcitlms.jpg",
-        "description": "Alternate profile viewer using Github API"
+        "img": "images/githubprofile.jpg",
+        "description": "Alternate profile viewer using Github API",
+        "tech": "ReactJS"
     },
     {
         "title": "Library Management System",
         "link": "http://lms-ashikul.rhcloud.com/",
         "img": "images/gcitlms.jpg",
-        "description": "A website for library borrowers, librarians, and administrators"
+        "description": "A website for library borrowers, librarians, and administrators",
+        "tech": "JSP, Spring MVC, SQL"
     },
     {
         "title": "Unique Brows",
         "link": "http://ashikul.github.io/UniqueBeauty/",
         "img": "images/uniquebrow.jpg",
-        "description": "Beauty Salon Website Design"
+        "description": "Beauty Salon Website Design",
+        "tech": "Bootstrap, Owl Carousel, HTML5, CSS3, JQuery"
     },
     {
         "title": "Harlem Karibe",
         "link": "http://ashikul.github.io/harlemkaribe/",
         "img": "images/harlemkaribe.jpg",
-        "description": "Caribbean Resturant Website Design"
+        "description": "Caribbean Resturant Website Design",
+        "tech": "HTML5, CSS3, JQuery"
     },
     {
         "title": "Secret Quotes",
         "link": "http://ashikul.github.io/secretquotes/",
         "img": "images/secretquotes.jpg",
-        "description": "Change your perspective, change your experience"
+        "description": "Change your perspective, change your experience",
+        "tech": "JQuery"
     },
     {
         "title": "Pomodoro Timer",
         "link": "http://ashikul.github.io/pomodoroclock/",
         "img": "images/pomodoro.jpg",
-        "description": "Time Management App. Work 25 Then Rest 5 And Repeat"
+        "description": "Time Management App. Work 25 Then Rest 5 And Repeat",
+        "tech": "AngularJS"
     },
     {
         "title": "HTML Calculator",
         "link": "http://ashikul.github.io/javascriptcalculator/",
         "img": "images/calculator.jpg",
-        "description": "Get the math done. Simple and fast"
+        "description": "Get the math done. Simple and fast",
+        "tech": "HTML"
     },
     {
         "title": "Local Weather",
         "link": "http://ashikul.github.io/localweather/",
         "img": "images/localweather.jpg",
-        "description": "Local weather app using the Open Weather API."
+        "description": "Local weather app using the Open Weather API.",
+        "tech": "AngularJS"
     },
     {
         "title": "Twitch TV",
         "link": "http://ashikul.github.io/twitchtv/",
         "img": "images/twitchtv.jpg",
-        "description": "Twitch tv app using the Twitch API"
+        "description": "Twitch tv app using the Twitch API",
+        "tech": "AngularJS"
     },
     {
         "title": "Camper News",
         "link": "http://ashikul.github.io/campernews/",
         "img": "images/campernews.jpg",
-        "description": "Displays the latest camper news stories using the camper API"
+        "description": "Displays the latest camper news stories using the camper API",
+        "tech": "AngularJS"
     },
     {
         "title": "Wikipedia Viewer",
         "link": "http://ashikul.github.io/wikipediaviewer/",
         "img": "images/wikipediaviewer.jpg",
-        "description": "Wikipedia viewer using the open wiki API"
+        "description": "Wikipedia viewer using the open wiki API",
+        "tech": "AngularJS"
     }
 ];
