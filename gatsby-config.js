4dc96f0b479f3cc0191b6dbdc0d227fb829b98b6
Ashik Alam <aalam@webmd.net>
2017-07-31 22:27:19 -0400

    adding public build.. NOTE: static images need ./

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     plugins: [`gatsby-plugin-react-helmet`, {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
-            trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+            trackingId: 'UA-71503474-1',
         },
     },],
 }
